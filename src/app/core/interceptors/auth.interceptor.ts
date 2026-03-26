a99214cb19012e3588890d81973afe994a97e97b
Sergio Barreras González <sergiobg.isc@gmail.com>
2026-03-25 23:25:10 -0600

    feat: enhance auth interceptor to handle 401 errors and improve token management

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,20 +1,28 @@
-import type { HttpInterceptorFn } from '@angular/common/http';
+import type { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { catchError, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
   const token = authService.getAccessToken();
 
-  if (!token) {
-    return next(req);
-  }
+  const request = token
+    ? req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      },
+    })
+    : req;
 
-  const clonedRequest = req.clone({
-    setHeaders: {
-      Authorization: `Bearer ${token}`
-    }
-  });
 
-  return next(clonedRequest);
+  return next(request).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401 && authService.getAccessToken()) {
+        authService.logout();
+      }
+
+      return throwError(() => error);
+    })
+  );
 };
