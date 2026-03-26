236ee52cd1a597b6f4f8c57e3656af5cb4f128c7
Sergio Barreras González <sergiobg.isc@gmail.com>
2026-03-25 21:46:10 -0600

    add routing configuration for authentication and dashboard components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./layout/auth-shell/auth-shell').then(m => m.AuthShell),
+        children: [
+            { path: 'login', loadComponent: () => import('./features/auth/pages/login/login').then(m => m.Login) },
+            { path: '', pathMatch: 'full', redirectTo: 'login' },
+        ],
+    },
+    {
+        path: '',
+        loadComponent: () => import('./layout/admin-shell/admin-shell').then(m => m.AdminShell),
+        children: [
+            { path: 'dashboard', loadComponent: () => import('./features/dashboard/pages/dashboard/dashboard').then(m => m.Dashboard) }
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: 'login',
+    }
+];
