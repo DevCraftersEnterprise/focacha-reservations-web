6dd5f0a6cd54071129dd5d83a50206291e784092
Sergio Barreras González <sergiobg.isc@gmail.com>
2026-03-25 21:36:06 -0600

    add initial Angular components and templates for login, dashboard, admin-shell, and auth-shell

---
+++ src/app/layout/admin-shell/admin-shell.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-shell',
+  imports: [],
+  templateUrl: './admin-shell.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AdminShell { }
