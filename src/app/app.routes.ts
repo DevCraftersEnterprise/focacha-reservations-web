f94e446fdc2f488a1be71015e306e4a055730572
Sergio Barreras González <sergiobg.isc@gmail.com>
2026-03-25 22:28:02 -0600

    feat: add authentication and guest guards for route protection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
+import { guestGuard } from './core/guards/guest.guard';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
     {
         path: '',
         loadComponent: () => import('./layout/auth-shell/auth-shell').then(m => m.AuthShell),
         children: [
-            { path: 'login', loadComponent: () => import('./features/auth/pages/login/login').then(m => m.Login) },
+            {
+                path: 'login',
+                canActivate: [guestGuard],
+                loadComponent: () => import('./features/auth/pages/login/login').then(m => m.Login)
+            },
             { path: '', pathMatch: 'full', redirectTo: 'login' },
         ],
     },
@@ -13,7 +19,11 @@ export const routes: Routes = [
         path: '',
         loadComponent: () => import('./layout/admin-shell/admin-shell').then(m => m.AdminShell),
         children: [
-            { path: 'dashboard', loadComponent: () => import('./features/dashboard/pages/dashboard/dashboard').then(m => m.Dashboard) }
+            {
+                path: 'dashboard',
+                canActivate: [authGuard],
+                loadComponent: () => import('./features/dashboard/pages/dashboard/dashboard').then(m => m.Dashboard)
+            }
         ]
     },
     {
---
+++ src/app/core/guards/guest.guard.ts
@@ -0,0 +1,14 @@
+import { Router, type CanActivateFn } from '@angular/router';
+import { AuthService } from '../services/auth';
+import { inject } from '@angular/core';
+
+export const guestGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.isAuthenticated()) {
+    return router.createUrlTree(['/dashboard']);
+  }
+
+  return true;
+};
